2f56c57f5bc7dcf4d96623cd61525ec4bc8b6607
vsratheesh <vsratheesh95@gmail.com>
2016-05-11 15:49:52 +0530

    EXPERIENCED YO

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,28 +8,31 @@
  *
  * Main module of the application.
  */
-angular
-  .module('mytodoApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+ angular
+   .module('mytodoApp', [
+     'ngAnimate',
+     'ngCookies',
+     'ngResource',
+     'ngRoute',
+     'ngSanitize',
+     'ngTouch',
+     'ui.sortable',
+     'LocalStorageModule'
+   ])
+   .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+     localStorageServiceProvider.setPrefix('ls');
+   }])
+   .config(function ($routeProvider) {
+     $routeProvider
+       .when('/', {
+         templateUrl: 'views/main.html',
+         controller: 'MainCtrl'
+       })
+       .when('/about', {
+         templateUrl: 'views/about.html',
+         controller: 'AboutCtrl'
+       })
+       .otherwise({
+         redirectTo: '/'
+       });
+   });
